46c9f3bd27861df3e558ebcbea7530f9f540a1d9
Shreya <svt23@njit.edu>
2022-12-21 19:03:47 +0500

    Accounts and transtion table scripts created

---
+++ app/Models/Account.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'account_number',
+        'account_type',
+        'user_id',
+        'balance'
+    ];
+}
---
+++ database/migrations/2022_12_21_134220_create_accounts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('account_number')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->string('balance')->default(0);
+            $table->string('account_type')->nullable();
+            $table->timestamps();
+        });
+
+        $user = \App\Models\User::whereName('shreya')->first();
+        \App\Models\Account::create([
+            'account_number' => '000000000000',
+            'user_id' => $user->id,
+            'account_type' => 'world',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
