c3626abee5e0679a7ca061c4c1a6f9e38ec7ad3b
Shreya <svt23@njit.edu>
2022-12-21 20:15:31 +0500

    working on dashboard

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,7 +4,7 @@
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
                     <span data-feather="home"></span>
-                    Home
+                    Dashboard
                 </a>
             </li>
             <li class="nav-item">
